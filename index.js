1320a8bd379c5b6e3df92001b1feae3de4fc2ac0
asiaromaria <designsbyarc@gmail.com>
2021-09-16 16:26:52 -0700

    installed expressed and added app.listener

---
+++ index.js
@@ -0,0 +1,15 @@
+const  express  =  require('express');
+
+
+const app = express();
+
+
+
+
+
+
+
+const port = process.env.PORT || 1000;
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
