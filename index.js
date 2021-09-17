a244559fd5b4fc32c4bb600d51a92dc8b5b8e7d5
asiaromaria <designsbyarc@gmail.com>
2021-09-16 20:10:36 -0700

    added routes

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const  express  =  require('express');
 const app = express();
 const connectDB = require('./startup/db');
+const cors = require('cors');
 
-
+const accounts = require('./routes/accounts');
 
 connectDB();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json()); 
+app.use('api/accounts', accounts)
 
 
 
-
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 2000;
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 });
