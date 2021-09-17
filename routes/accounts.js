33dfe32a9800e3a82f1dfce9133cf8b233dad38e
asiaromaria <designsbyarc@gmail.com>
2021-09-16 20:11:22 -0700

    trying to fix crud, not allowing to post

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,2 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const { User, validateUser, userSchema } = require('../models/account');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        return res.send(users);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
+router.post('/', async (req, res) => {
+    try {
+        // const { error } = validateUser(req.body);
+        // if (error) return res.status(400).send(error.details[0].message);
+        // // let user = await User.findOne({ email: req.body.email });
+        // if (user) return res.status(400).send('User already registered.');
+        // const salt = await bcrypt.genSalt(10);
+
+            // user = new User({
+            // name: req.body.name,
+            // email: req.body.email,
+            // password: req.body.password,
+            // password: await bcrypt.hash(req.body.password, salt),
+            // isAdmin: req.body.isAdmin,
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+        await user.save();
+        // const token = user.generateAuthToken();
+        return res
+        .send(user);
+        // .header('x-auth-token', token)
+        // .header('access-control-expose-headers', 'x-auth-token')
+        // .send({ _id: user._id, name: user.name, email: user.email });
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.put('/:userId', async (req, res) => {
+    try {
+        const { error } = validateUser(req.body);
+        if (error) return res.status(400).send(error);
+
+        const user = await User.findByIdAndUpdate(req.params.userId);
+        if (!user) return res.status(400).send(`The user with id "${req.params.userId}" does not exist.`);
+
+        user.name = req.body.name;
+        user.email = req.body.email;
+        user.password = req.body.password;
+
+        await user.save();
+        return res.send(user);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+// router.delete('/:userId', async, (req, res) => {
+//     try {
+//         const user = await User.findByIdAndRemove(req.params.userId);
+//         if (!user) return res.status(400).send(`The user with id "${req.params.userId}" could not be found.`);
+//         await user.save();
+//         return res.send(user);
+//     }   catch (ex) {
+//         return res.status(500).send(`Internal Server Error: ${ex}`);
+//     }
+// });
+
+module.exports = router;
