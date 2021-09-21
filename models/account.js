e46810591f8cdf0281d14d81dbb12fda8b343f75
asiaromaria <designsbyarc@gmail.com>
2021-09-21 16:47:43 -0700

    added jwt & generateAuthToken function

--- models/account.js
+++ models/account.js
@@ -1,16 +1,19 @@
 const  mongoose  =  require('mongoose');
 const Joi = require('joi');
 const config = require('config');
+const jwt = require('jsonwebtoken');
 
 
  const  userSchema  =  new  mongoose.Schema({ 
     name:  { type: String, required: true, minlength: 5, maxlength: 50 },
     email: { type: String, unique: true, required: true, minlength: 5, maxlength: 255 },
     password: { type: String, required: true, minlength: 5, maxlength: 1024 },
-    // isAdmin: { type: Boolean, default: false},
+    isAdmin: { type: Boolean, default: false},
     }); 
 
-
+    userSchema.methods.generateAuthToken = function () {
+        return jwt.sign({ _id: this._id, name: this.name }, config.get('jwtSecret'));
+       };
     const User = mongoose.model('User', userSchema);
 
     function validateUser(user) {
