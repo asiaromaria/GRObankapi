c90b0c1a5660aca9b1ec090495ba3f4d52a8a653
asiaromaria <designsbyarc@gmail.com>
2021-09-27 19:09:59 -0700

    took out balance route, not neccessary

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 
 const accounts = require('./routes/accounts');
 const auth = require('./routes/auth');
-const balances = require('./routes/balances');
 
 connectDB();
 
@@ -13,7 +12,6 @@ app.use(cors());
 app.use(express.json()); 
 app.use('/api/auth', auth)
 app.use('/api/accounts', accounts)
-app.use('/api/balance', balances)
 
 
 
